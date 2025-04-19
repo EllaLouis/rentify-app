a09098c3b3141b5dd4c18609c62193868daac820
EllaElogbamien <153170132+EllaLouis@users.noreply.github.com>
2025-04-19 00:42:13 -0600

    Rentify

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
+import Image from 'next/image';
 import Footer from "./components/Footer";
 
 function App() {
@@ -20,17 +21,20 @@ function App() {
         <h2 className="text-2xl font-bold mb-5">Rent the Perfect Event Supplies!</h2>
         <p className="mb-4">
           Rentify event rentals and entertainment, deal on varieties of unique rental items for any event.
-          We're ready to help you make your event an unforgettable one, with our wide range of high-quality rental items.
+          We&apos;re ready to help.
+          help you make your event an unforgettable one, with our wide range of high-quality rental items.
           From chairs, tables, linens, decor and even lightings, we have everything you need to make your event a success.
         </p>
 
         <div className="">
           {items.map((item) => (
             <div key={item.id} className="border p-4 rounded shadow text-center">
-              <img
+              <Image
                 src={item.imageUrl}
                 alt={item.name}
-                className="w-full h-40 object-cover rounded mb-2"
+                width={400}
+                height={200}
+                className="rounded mb-2 object-cover"
               />
               <h2 className="text-xl font-semibold">{item.name}</h2>
             </div>
