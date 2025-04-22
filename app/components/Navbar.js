3ded58c3ea2b216427def15931815f860676f3c5
EllaElogbamien <153170132+EllaLouis@users.noreply.github.com>
2025-04-21 21:54:43 -0600

    Rentify

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Link from "next/link";
-import SearchBar from "./components/SearchBar";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => {
     return (
